4063b58a7dbebc178d9d31a58649b872ec212522
mashah8488 <mashah@visa.com>
2015-06-22 16:28:15 -0700

    Initial Commit

---
+++ FacialRecognitionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacialRecognitionDemo
+//
+//  Created by Shah, Mansi on 6/22/15.
+//  Copyright (c) 2015 Shah, Mansi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
